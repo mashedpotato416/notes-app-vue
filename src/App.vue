cf35f74705ee86a64f1440755c69bb4b52b1708b
Hector Vergara <hector.vergara416@gmail.com>
2020-11-20 22:58:46 +0800

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,93 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <h1 class="header">Notes App</h1>
+      <div class="login-prompt">You are currently logged in as hector.vergara416@gmail.com</div>
+      <button class="logout-button" @click="testMethodApp">Logout</button>
+      <div v-for="note in Notes" :key="note.dataId">
+        <notes-main
+        :noteUser="note.dataUser"
+        :noteTitle="note.dataTitle"
+        :noteContent="note.dataContent">
+        </notes-main>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NotesMain from './components/NotesMain.vue'
+import uniqueId from 'lodash.uniqueid';
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    NotesMain
+  },
+  data() {
+    return {
+      Notes: [
+          {
+            dataUser: 'steve_austin@gmail.com',
+            dataId: uniqueId('note-'),
+            dataTitle: "Does anybody have, a cold beer for Steve Austin?!??!!?",
+            dataContent: "I'll open up a can of whoop-ass on you!” “That was the absolute worst catch phrase I've ever heard in the history of Monday Night Raw.” “Last time I had three or four of those I crapped for three days straight!"
+          },
+          {
+            dataUser: 'john_cena@yahoo.com',
+            dataId: uniqueId('note-'),
+            dataTitle: "Loyalty",
+            dataContent: "When people show loyalty to you, you take care of those who are with you. It's how it goes with everything. If you have a small circle of friends, and one of those friends doesn't stay loyal to you, they don't stay your friend for very long."
+          }
+        ]
+    };
+  },
+  methods: {
+    testMethodApp() {
+      console.log(this.Notes[0]['dataUser'])
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  font-family: 'Source Sans Pro', sans-serif;
   color: #2c3e50;
   margin-top: 60px;
+}
+.container {
+  margin: auto;
+  width: 50%;
+  border: 3px solid #4bf8f8;
+  padding: 0px;
+  background-color: #4fffff30;
+  border-radius: 5px;
+  text-align: center;
+}
+.header {
+  position: relative;
+  font-size: 3em;
+  margin-bottom: 5px;
+}
+.logout-button {
+  border-color: #ffffff;
+  padding: 5px;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 2px 2px;
+  cursor: pointer;
+  width: 20%;
+  height: 30px;
+  background-color: #ffffff;
+  border-radius: 12px;
+  text-transform: uppercase;
+  font-size: 0.75em;
+}
+.login-prompt {
+  text-align: center;
+  font-size: 1em;
+  margin: 5px;
 }
 </style>
---
+++ src/components/NotesMain.vue
@@ -0,0 +1,85 @@
+<template>
+<div>
+  <ul>
+    <li>
+      <div class="note-title"><strong>{{ noteTitle }}</strong></div>
+      <div class="note-content">{{ noteContent }}</div>
+      <div class="note-user"><em>by: {{ noteUser }}</em></div>
+      <div class="note-buttons">
+        <button class="button note-button">Edit</button>
+        <button class="button note-button delete-button" @click="testMethod">Delete</button>
+      </div>
+    </li>
+  </ul>
+</div>
+</template>
+<script>
+export default {
+  props: {
+      noteId: { type: String },
+      noteTitle: { type: String },
+      noteContent: { type: String },
+      noteUser: { type: String }
+    },
+  methods: {
+    testMethod() {
+      console.log("Test2");
+    }
+  }
+}  
+</script>
+<style>
+  .button {
+  border-color: #ffffff;
+  padding: 5px;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 2px 2px;
+  cursor: pointer;
+  width: 20%;
+  height: 30px;
+  background-color: #ffffff;
+  border-radius: 12px;
+  text-transform: uppercase;
+  font-size: 0.75em;
+  }
+  ul {
+    display: inline-block;
+    width: 95%;
+    background-color: #03f4f4ab;
+    list-style-type: none;
+    margin: 5px;
+    padding: 5px;
+    border-radius: 12px;
+  }
+  li {
+    text-align: left;
+  }
+  .note-title {
+    padding: 8px;
+  }
+  .note-content {
+    padding: 8px;
+    text-align: justify;
+    text-justify: inter-word;
+  }
+  .note-user {
+    text-align: right;
+    padding: 8px;
+  }
+  .note-buttons {
+    padding: 8px;
+    text-align: center;
+  }
+  /* to center note-buttons */
+  .note-button {
+    margin: 5px 30px 5px 30px;
+  }
+  .delete-button {
+    background-color: red;
+    border-color: red;
+    color: #ffffff;
+  }
+
+</style>
